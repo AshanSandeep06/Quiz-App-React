57476aad471f39ab1375e760ffe1bde8dc599121
AshanSandeep06 <ashansandeep06@gmail.com>
2023-04-05 23:28:13 +0530

    Content Updated in App Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Paper } from "@mui/material";
 
 const App = () => {
   return (
     <div id="App" className="bg-customColors-100 w-full h-screen">
       <Paper elevation={3}>
-      <Paper/>
+        
+      </Paper>
     </div>
   );
 };
