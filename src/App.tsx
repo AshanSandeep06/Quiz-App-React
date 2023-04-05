a1df69bc90f94d845c935e1657dcc9c2beee2968
AshanSandeep06 <ashansandeep06@gmail.com>
2023-04-06 00:16:50 +0530

    Content Updated

--- src/App.tsx
+++ src/App.tsx
@@ -9,63 +9,83 @@ import {
   Paper,
   Radio,
   RadioGroup,
+  ThemeProvider,
+  createTheme,
 } from "@mui/material";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Poppins, sans-serif",
+  },
+});
 
 const App = () => {
   return (
-    <div
-      id="App"
-      className="bg-customColors-100 w-full 
+    <ThemeProvider theme={theme}>
+      <div
+        id="App"
+        className="bg-customColors-100 w-full 
       h-screen flex items-center justify-center"
-    >
-      <Paper
-        elevation={3}
-        className="w-1/2 h-3/4
+      >
+        <Paper
+          elevation={3}
+          className="w-1/2 h-3/4
         flex flex-col
         items-center p-10 !rounded-xl !font-poppins"
-      >
-        <div>
-          <h1 className="">Question 01</h1>
-        </div>
+        >
+          <div>
+            <h1 className="">Question 01</h1>
+          </div>
 
-        <div>
-          <p>Your Question</p>
-        </div>
+          <div>
+            <p>Your Question</p>
+          </div>
 
-        <div>
-          <FormControl>
-            <RadioGroup defaultValue="" name="rdbButtonsGroup">
-              <FormControlLabel
-                value="answer1"
-                control={<Radio />}
-                label="Answer 01"
-              />
-              <FormControlLabel
-                value="answer2"
-                control={<Radio />}
-                label="Answer 02"
-              />
-              <FormControlLabel
-                value="answer3"
-                control={<Radio />}
-                label="Answer 03"
-              />
-              <FormControlLabel
-                value="answer4"
-                control={<Radio />}
-                label="Answer 04"
-              />
-            </RadioGroup>
-          </FormControl>
-        </div>
+          <div>
+            <FormControl>
+              <RadioGroup defaultValue="" name="rdbButtonsGroup">
+                <FormControlLabel
+                  value="answer1"
+                  control={<Radio />}
+                  label="Answer 01"
+                  className="!font-poppins"
+                />
+                <FormControlLabel
+                  value="answer2"
+                  control={<Radio />}
+                  label="Answer 02"
+                  className="!font-poppins"
+                />
+                <FormControlLabel
+                  value="answer3"
+                  control={<Radio />}
+                  label="Answer 03"
+                  className="!font-poppins"
+                />
+                <FormControlLabel
+                  value="answer4"
+                  control={<Radio />}
+                  label="Answer 04"
+                  sx={{
+                    fontFamily: "Poppins",
+                  }}
+                />
+              </RadioGroup>
+            </FormControl>
+          </div>
 
-        <div>
-          <Button variant="contained" color="success">
-            Success
-          </Button>
-        </div>
-      </Paper>
-    </div>
+          <div>
+            <Button
+              variant="contained"
+              color="success"
+              className="!font-poppins"
+            >
+              Success
+            </Button>
+          </div>
+        </Paper>
+      </div>
+    </ThemeProvider>
   );
 };
 
