bfe15df7e619beb3c314ce67f58182d455ea4f52
AshanSandeep06 <ashansandeep06@gmail.com>
2023-04-05 23:33:05 +0530

    Content Updated in App Component

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,16 @@ import { Paper } from "@mui/material";
 
 const App = () => {
   return (
-    <div id="App" className="bg-customColors-100 w-full h-screen">
-      <Paper elevation={3}>
-        
-      </Paper>
+    <div
+      id="App"
+      className="bg-customColors-100 w-full 
+      h-screen flex items-center justify-center"
+    >
+      <Paper
+        elevation={3}
+        className="w-1/3 h-1/2 !bg-slate-900 
+        flex flex-col justify-between items-center p-10"
+      ></Paper>
     </div>
   );
 };
