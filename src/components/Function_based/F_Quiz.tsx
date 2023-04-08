5d117f5b91e6a0378add81f82e51eb9a40b882ef
AshanSandeep06 <ashansandeep06@gmail.com>
2023-04-09 00:32:58 +0530

    Content Updated in functional component

--- src/components/Function_based/F_Quiz.tsx
+++ src/components/Function_based/F_Quiz.tsx
@@ -13,22 +13,22 @@ export const F_Quiz = () => {
     <Paper
       elevation={3}
       className="w-1/2 h-3/4
-  flex flex-col
-  items-center p-10 !rounded-xl !font-poppins"
+flex flex-col
+items-center p-10 !rounded-xl !font-poppins"
       sx={{ gap: 10 / 8 }}
     >
-      <div className="mb-[3px]">
+      <div className="mb-[12px] mt-[3px]">
         <h1 className="text-[25px] font-semibold">Question 01</h1>
       </div>
 
-      <div className="mb-[15px]">
+      <div className="mb-[24px]">
         <p className="text-center text-xl font-medium">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas quidem
           similique soluta a deserunt neque ?
         </p>
       </div>
 
-      <div className="self-start pl-[13px]">
+      <div className="self-start pl-[13px] mb-[16px]">
         <FormControl>
           <RadioGroup defaultValue="" name="rdbButtonsGroup">
             <FormControlLabel
@@ -55,9 +55,13 @@ export const F_Quiz = () => {
         </FormControl>
       </div>
 
-      <div>
-        <Button variant="contained" color="success" className="!font-poppins">
-          Success
+      <div className="w-1/4">
+        <Button
+          variant="contained"
+          fullWidth={true}
+          sx={{ fontFamily: "Poppins, sans-serif", bgcolor: "navy" }}
+        >
+          Submit Answer
         </Button>
       </div>
     </Paper>
