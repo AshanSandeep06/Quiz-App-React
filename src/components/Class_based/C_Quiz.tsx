796d25528312db274ae5e1426227763287380e4d
AshanSandeep06 <ashansandeep06@gmail.com>
2023-04-09 00:04:41 +0530

    Content Updated

--- src/components/Class_based/C_Quiz.tsx
+++ src/components/Class_based/C_Quiz.tsx
@@ -18,11 +18,11 @@ export default class C_Quiz extends Component {
     items-center p-10 !rounded-xl !font-poppins"
         sx={{ gap: 10 / 8 }}
       >
-        <div className="mb-[3px]">
+        <div className="mb-[8px]">
           <h1 className="text-[25px] font-semibold">Question 01</h1>
         </div>
 
-        <div className="mb-[15px]">
+        <div className="mb-[24px]">
           <p className="text-center text-xl font-medium">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas
             quidem similique soluta a deserunt neque ?
