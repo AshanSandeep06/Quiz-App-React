9176f0df690020b22bb26a47542a6707f20a9aed
AshanSandeep06 <ashansandeep06@gmail.com>
2023-04-09 15:00:12 +0530

    Content Updated

--- src/components/Class_based/C_Quiz.tsx
+++ src/components/Class_based/C_Quiz.tsx
@@ -121,13 +121,9 @@ export default class C_Quiz extends Component<C_QuizProps, C_QuizState> {
 
         {
           id: "08",
-          question: "Who had the hard-hitting Five star match at WrestleMania 39 goes Hollywood?",
-          answers: [
-            "Seth Rollins",
-            "Brock Lesnar",
-            "Gunther",
-            "John Cena",
-          ],
+          question:
+            "Who had the hard-hitting Five star match at WrestleMania 39 goes Hollywood?",
+          answers: ["Seth Rollins", "Brock Lesnar", "Gunther", "John Cena"],
           correctAnswer: "Gunther",
           submittedAnswer: "",
           answerIsCorrect: false,
