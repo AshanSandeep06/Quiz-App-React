5010973b8d04330951a05fd9bd1b9d07847c78ec
AshanSandeep06 <ashansandeep06@gmail.com>
2023-04-09 17:12:07 +0530

    Importing JQuery to the C_Quiz class Component

--- src/components/Class_based/C_Quiz.tsx
+++ src/components/Class_based/C_Quiz.tsx
@@ -10,6 +10,7 @@ import {
 } from "@mui/material";
 import React, { Component } from "react";
 import { questionDetails } from "../../types/QuestionDetails";
+import $ from 'jquery';
 
 const theme = createTheme({
   typography: {
