9c96e11ef2595188c48e55c22348f0db6b060357
AshanSandeep06 <ashansandeep06@gmail.com>
2023-04-09 14:28:11 +0530

    Content Updated

--- src/components/Class_based/C_Quiz.tsx
+++ src/components/Class_based/C_Quiz.tsx
@@ -89,6 +89,16 @@ export default class C_Quiz extends Component<C_QuizProps, C_QuizState> {
           submittedAnswer: "",
           answerIsCorrect: false,
         },
+
+        {
+          id: "06",
+          question:
+            "Who is the better in-ring Technician among of the wrestlers in WWE?",
+          answers: ["Roman Reigns", "Seth Rollins", "Undertaker", "John Cena"],
+          correctAnswer: "Seth Rollins",
+          submittedAnswer: "",
+          answerIsCorrect: false,
+        },
       ],
     };
   }
