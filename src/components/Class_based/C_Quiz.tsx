8ee550d4a2df829780bc7f8dd4fc1b7e7e082221
AshanSandeep06 <ashansandeep06@gmail.com>
2023-04-09 17:17:23 +0530

    Content Updated

--- src/components/Class_based/C_Quiz.tsx
+++ src/components/Class_based/C_Quiz.tsx
@@ -271,9 +271,7 @@ export default class C_Quiz extends Component<C_QuizProps, C_QuizState> {
   }
 
   handleClickSubmit = () => {
-    if(){
-      console.log("Clicked..!");
-    }
+   
   };
 
   render() {
