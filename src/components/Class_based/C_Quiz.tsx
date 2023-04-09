33e558bdb1fdc794c0c78c8a648a8b393c9d5506
AshanSandeep06 <ashansandeep06@gmail.com>
2023-04-09 15:51:05 +0530

    Content Updated with creating Questions and answers

--- src/components/Class_based/C_Quiz.tsx
+++ src/components/Class_based/C_Quiz.tsx
@@ -179,9 +179,9 @@ export default class C_Quiz extends Component<C_QuizProps, C_QuizState> {
         {
           id: "14",
           question:
-            "?",
-          answers: ["Kurt Angle", "Roman Reigns", "Undertaker", "Seth Rollins"],
-          correctAnswer: "Seth Rollins",
+            "Who is the WWE Wrestler had beat Brock Lesnar in Wrestlemania 2020 under Five Minutes ?",
+          answers: ["Eddie Guevero", "Kurt Angle", "Drew Mcintyre", "Seth Rollins"],
+          correctAnswer: "Drew Mcintyre",
           submittedAnswer: "",
           answerIsCorrect: false,
         },
