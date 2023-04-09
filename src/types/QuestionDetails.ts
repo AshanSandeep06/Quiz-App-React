3ad6a5a72b1997e07d1c1faa0457df0df3458abf
AshanSandeep06 <ashansandeep06@gmail.com>
2023-04-09 18:01:55 +0530

    Content Updated

--- src/types/QuestionDetails.ts
+++ src/types/QuestionDetails.ts
@@ -3,6 +3,8 @@ export type questionDetails = {
     question: string;
     answers: string[];
     correctAnswer: string;
+    correctAnswerIndex?: number;
     submittedAnswer: string;
+    submittedAnswerIndex?: number;
     answerIsCorrect: boolean;
   };
