81c738894ffc4859f2e403be7866a1ead0b5fe6a
AshanSandeep06 <ashansandeep06@gmail.com>
2023-04-09 19:49:13 +0530

    Content Updated in C_Quiz Component

--- src/components/Class_based/C_Quiz.tsx
+++ src/components/Class_based/C_Quiz.tsx
@@ -340,7 +340,10 @@ export default class C_Quiz extends Component<C_QuizProps, C_QuizState> {
         prevState.questionList[0].answers[3],
       ],
       correctAnswer: prevState.questionList[0].correctAnswer,
-      correctAnswerIndex: prevState.questionList[0].correctAnswerIndex
+      correctAnswerIndex: prevState.questionList[0].correctAnswerIndex,
+      submittedAnswer: "",
+      submittedAnswerIndex: 0,
+      answerIsCorrect: false,
     }));
   }
 
