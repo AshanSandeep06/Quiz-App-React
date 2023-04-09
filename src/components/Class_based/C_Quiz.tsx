5271ed7de4cd70670f29f15eeeb06d9f06a15937
AshanSandeep06 <ashansandeep06@gmail.com>
2023-04-09 19:43:57 +0530

    Content Updated in C_Quiz Component

--- src/components/Class_based/C_Quiz.tsx
+++ src/components/Class_based/C_Quiz.tsx
@@ -333,8 +333,12 @@ export default class C_Quiz extends Component<C_QuizProps, C_QuizState> {
       ...prevState,
       id: "0" + ++questionID,
       question: prevState.questionList[0].question,
-      answers: [prevState.questionList[0].answers[0], prevState.questionList[0].answers[1], prevState.questionList[0].answers[2], prevState.questionList[0].answers[3]]
-      
+      answers: [
+        prevState.questionList[0].answers[0],
+        prevState.questionList[0].answers[1],
+        prevState.questionList[0].answers[2],
+        prevState.questionList[0].answers[3],
+      ],
     }));
   }
 
