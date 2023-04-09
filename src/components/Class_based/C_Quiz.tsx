295aac3c93103cfdda5cdada76666fcf4efded5e
AshanSandeep06 <ashansandeep06@gmail.com>
2023-04-09 18:19:58 +0530

    Content Updated

--- src/components/Class_based/C_Quiz.tsx
+++ src/components/Class_based/C_Quiz.tsx
@@ -314,6 +314,7 @@ export default class C_Quiz extends Component<C_QuizProps, C_QuizState> {
     if ($("input[name='answer3'][value='3']").prop("checked")) {
       console.log("Clicked..!");
       //How to get a value of a radio button when it is checked
+      // $("input[name='answer3'][value='3']").prop("checked")
       // console.log($('input[name="answer3"]:checked').val());
       // console.log($('input[name="answer3"]:checked').parent().parent().children(":eq(1)").text());
     }
